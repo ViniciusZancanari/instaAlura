4be3a37367efa6dc26656039552c54aef524ea7e
Vinicius Zancanari <viniciuszanca@hotmail.com>
2021-01-21 09:21:57 -0300

    [add] Comments post

--- src/components/comments/comments.js
+++ src/components/comments/comments.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import {     
-        FlatList,
-        Text      
-} from 'react-native';
+  
+import React, { Fragment, useState } from 'react';
+import {
+    Image,
+    Text,
+    TouchableOpacity,
+    View,
+    FlatList,
+    TextInput
+} from "react-native";
 
-const Comments = ( {comments} ) => {
+import screenStyle from './style';
+
+const Comments = ({ comments }) => {
+    const [stateComments, setComments] = useState(comments);
+
+
+    const addComment = () => {
+        inputClear.clear();
+        const newComment = {
+            date: Date.now(),
+            text: inputComment,
+            userName: "vini_zanca"
+        }
+
+        setComments([...stateComments, newComment]);
+    }
+
+
+    let inputClear;
+    let inputComment = "";
     return (
-        <FlatList
-        data = {comments}
-        keyExtractor = {(item, index) => index.toString()}
-        renderItem = {({item}) => 
-            <Text>{item.text}</Text>}
-        />
+        <Fragment>
+
+            <FlatList
+                data={stateComments}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) =>
+                <View style={screenStyle.inline}>
+                    <Text>{item.userName} </Text>
+                    <Text> {item.text}</Text>
+                </View>
+                }
+            />
+            <View style={screenStyle.inline}>
+                <TextInput
+                    ref={textInput => inputClear = textInput}
+                    onChangeText={text => inputComment = text}
+                    placeholder={"Deixe seu comentário..."}
+                    style={{ flex: 1 }}
+                />
+                <TouchableOpacity onPress={addComment}>
+                    <Image source={require("../../../res/img/send.png")}
+                        style={screenStyle.imgSend} />
+                </TouchableOpacity>
+            </View>
+        </Fragment>
     );
 };
+
 export default Comments;
